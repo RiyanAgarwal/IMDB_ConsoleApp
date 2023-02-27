83fb6ff6513d9a6e3603693f3781cde85f8617b7
Riyan <agarwalriyan200116@gmail.com>
2023-02-27 16:39:58 +0530

    Add the files

---
+++ Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    public class Calculator : ICalculator
+    {
+        private float _result;
+
+        public void SetResult(float result)
+        {
+            this._result = result;
+        }
+        public virtual float GetResult()
+        {
+            return _result;
+        }
+        public int Add(int a, int b)
+        {
+            var result = a + b;
+            this.SetResult(Convert.ToSingle(result));
+            return result;
+
+        }
+        public int Add(int a, int b, int c)
+        {
+            var result = a + b + c;
+            this.SetResult(Convert.ToSingle(result));
+            return result;
+        }
+
+        public float Add(float a, float b)
+        {
+            var result = a + b;
+            this.SetResult(result);
+            return result;
+        }
+
+    }
+}
---
+++ program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+            var sumOfTwoInt = calc.Add(12, 6);
+            Console.WriteLine("sum of two integers = " + Convert.ToString(sumOfTwoInt));
+            Console.WriteLine("value of result in Calculator class = " + Convert.ToString(calc.GetResult()));
+
+            var sumOfThreeInt = calc.Add(6, 88, 2);
+            Console.WriteLine("sum of three integers = " + Convert.ToString(sumOfThreeInt));
+            Console.WriteLine("value of result in Calculator class = " + Convert.ToString(calc.GetResult()));
+
+
+            var sumOfFloat = calc.Add(13.6f, 44.8f);
+            Console.WriteLine("sum of two floats = " + Convert.ToString(sumOfFloat));
+            
+            
+
+            Console.WriteLine("value of result in Calculator class = " + Convert.ToString(calc.GetResult()));
+            var advCalc = new AdvanceCalculator();
+            var power = advCalc.Power(3, 5);
+            Console.WriteLine("result of power method = " + Convert.ToString(power));
+            Console.WriteLine("value of result in AdvanceCalculator class = " + Convert.ToString(advCalc.GetResult()));
+
+        }
+    }
+}
