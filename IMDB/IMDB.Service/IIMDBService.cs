786e05d98782cf9ef364d793ffe6e147e9311591
Riyan Agarwal <agarwalriyan200116@gmail.com>
2023-03-13 06:23:20 +0000

    Remove unnecessary comments

--- IMDB/IMDB.Service/IIMDBService.cs
+++ IMDB/IMDB.Service/IIMDBService.cs
@@ -4,7 +4,7 @@ namespace IMDB.Service
 {
     public interface IIMDBService
     {
-        //void Add(int yearOfRelease, string name, string plot);
+       
         void Add(int yearOfRelease, string name, string plot, string producer, List<string> actors);
         List<string> ChosenActors(string index);
         string ChosenProducer(int index);
